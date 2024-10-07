7352ff2e8dcb983eb490a17505e9904494d829e8
Ibarra Hector Leonel <ibarra.h.leonel@gmail.com>
2024-10-07 11:56:29 -0300

    Refactoring login generator passwords

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from "react"
-
-const generator = () => {
-  const password = Math.random().toString(36).slice(-8)
-  return password
-}
+import usePasswordGenerator from "./hooks/PasswordGenerator"
 
 export default function App() {
+  const result = usePasswordGenerator()
   const [password, setPassword] = useState('')
   return (
     <div>
       <p>Password Generator</p>
-      <button onClick={() => setPassword(generator())}>Generate</button>
+      <button onClick={() => setPassword(result)}>Generate</button>
       <div>{password}</div>
       <button>Copy</button>
     </div>
