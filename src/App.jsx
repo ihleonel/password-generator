4fdd1cccd85fa5c5e53667c773cf0e41e2fd79aa
Ibarra Hector Leonel <ibarra.h.leonel@gmail.com>
2025-08-02 16:56:21 -0300

    Feat: Incomplete generator password

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react"
 import usePasswordGenerator from "./hooks/PasswordGenerator"
+import { use } from "react"
+
+const generate = (length, special) => {
+  let result = usePasswordGenerator(length, special)
 
 const copy = (text) => {
   navigator.clipboard.writeText(text)
@@ -9,23 +13,27 @@ export default function App() {
   const result = usePasswordGenerator()
   const [password, setPassword] = useState('')
   const [special, setSpecial] = useState(false)
+  const [length, setLength] = useState(8)
   return (
     <>
       <h3>Password Generator</h3>
       <label>
         <input
           type="checkbox"
-          id="special"
-          name="special"
           value={special}
           onChange={() => setSpecial(!special)}/>
         Include special characters
       </label>
       <label htmlFor="length">Length</label>
-      <input type="number" className="input" id="length" name="length" value={result.length} readOnly/>
+      <input
+        type="number"
+        className="input"
+        value={length}
+        onChange={(e) => setLength(e.target.value)}
+      />
       <button
         className="btn-primary"
-        onClick={() => setPassword(result)}
+        onClick={() => generate(length, special)}
       >
         Generate
       </button>
